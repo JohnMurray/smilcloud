5a9d887266f70a7035754e5b07f32493635b14f9
John Murray <murrayj5@nku.edu>
2011-04-17 17:13:03 -0400

    added icon, new application-level-methods, more progress on Player

--- src/edu/nku/cs/csc440/team2/SMILCloud.java
+++ src/edu/nku/cs/csc440/team2/SMILCloud.java
@@ -11,9 +11,13 @@ public class SMILCloud extends Application
   private ArrayList<MessageLite> messages;
 	
   private String queuedDocumentToPlayID;
+  private String queueDocumentToEditID;
 	
 	public void queueDocumentToPlay(String id) { this.queuedDocumentToPlayID = id; }
 	public String getQueuedDocumentForPlayback() { return this.queuedDocumentToPlayID; }
+	
+	public void queueDocumentToEdit(String id) { this.queueDocumentToEditID = id; }
+	public String getQueuedDocumentForEditing() { return this.queueDocumentToEditID; }
 
   public synchronized void addMessage(MessageLite message)
   {
