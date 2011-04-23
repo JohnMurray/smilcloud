ccb082971d4ea3cc27ae0099c02499c8465f31a1
John Murray <murrayj5@nku.edu>
2011-04-22 23:24:45 -0400

    uploader up and running (just without the uploading yet)

--- src/edu/nku/cs/csc440/team2/uploader/Uploader.java
+++ src/edu/nku/cs/csc440/team2/uploader/Uploader.java
@@ -4,12 +4,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import edu.nku.cs.csc460.team2.R;
 
 public class Uploader extends Activity 
 {
 
+	private Uri uri;
+	private EditText fileLocationText;
+	
 	/**
      * @param savedInstanceState
      * @return void
@@ -20,10 +27,27 @@ public class Uploader extends Activity
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-    	setContentView(R.layout.inbox_main);
-    	Intent intent = new Intent("org.openintents.action.PICK_FILE");
-    	startActivityForResult(intent, 1);
+    	setContentView(R.layout.uploader_main);
     	
+    	fileLocationText = (EditText)findViewById(R.id.uploader_edit_text);
+    	Button findMedia = (Button)findViewById(R.id.uploader_select_file_button);
+    	Button upload = (Button)findViewById(R.id.uploader_upload_file_button);
+    	
+    	
+    	findMedia.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent("org.openintents.action.PICK_FILE");
+		    	startActivityForResult(intent, 1);
+			}
+		});
+    	
+    	upload.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
     }
     
     @Override
@@ -33,11 +57,11 @@ public class Uploader extends Activity
     	
     	if( resultCode == RESULT_OK && data != null )
     	{
-    		Uri fileUri = data.getData();
-    		if( fileUri != null)
+    		this.uri = data.getData();
+    		if( this.uri != null)
     		{
-    			String filePath = fileUri.getPath();
-    			Toast.makeText(this, filePath, Toast.LENGTH_LONG);
+    			String filePath = this.uri.getPath();
+    			this.fileLocationText.setText(filePath);
     		}
     	}
     }
